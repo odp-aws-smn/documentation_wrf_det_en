e13f2c207601b1abf2369d75fe50899c9e59c2c1
odp-aws-smn <102237894+odp-aws-smn@users.noreply.github.com>
2022-04-04 15:42:36 -0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -27,7 +27,7 @@ show_authors = True
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme', 'myst_parser']
+extensions = ['sphinx_rtd_theme', 'myst_parser','sphinx_toolbox.sidebar_links']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
